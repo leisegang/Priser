0171de2d14fb6e141e453ead031270ecaf139d2c
Anders <arodal91@gmail.com>
2014-10-26 12:42:07 +0100

    Lagt til forsidebile og endret layout på knapper

--- gen/com/example/androidhive/R.java
+++ gen/com/example/androidhive/R.java
@@ -11,13 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button=0x7f020000;
+        public static final int forside=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int btnCreateProduct=0x7f050003;
         public static final int btnDelete=0x7f050005;
         public static final int btnSave=0x7f050004;
-        public static final int btnViewProducts=0x7f050009;
+        public static final int btnViewProducts=0x7f05000a;
+        public static final int imageView1=0x7f050009;
         public static final int inputDesc=0x7f050002;
         public static final int inputName=0x7f050000;
         public static final int inputPrice=0x7f050001;
