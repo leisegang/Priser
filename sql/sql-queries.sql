9820fb33d9b2404f0127e49d75ef644fa2f4e7f5
leisegang <leisegang@gmail.com>
2014-11-03 17:22:01 +0100

    Oppdaterte SQL

--- sql/sql-queries.sql
+++ sql/sql-queries.sql
@@ -6,4 +6,29 @@ ORDER BY Customers.CustomerName;
 
 
 
+SELECT utested.uid, utested.name, utested.description, utested.url, utested.picurl, utested.mapurl, price.price
+FROM utested
+INNER JOIN price
+ON utested.uid=price.uid
+GROUP BY utested.uid
+ORDER BY utested.uid ASC;
+
+
+SELECT utested.uid, utested.name, utested.description, utested.url, utested.picurl, utested.mapurl, price.price
+FROM utested
+INNER JOIN price
+ON utested.uid=price.uid
+ORDER BY price.price ASC;
+
+SELECT utested.uid, utested.name, utested.description, utested.url, utested.picurl, utested.mapurl, price.price, price.pid
+FROM utested
+INNER JOIN price
+ON utested.uid=price.uid
+GROUP BY utested.uid
+ORDER BY price.pid DESC;
+
+
+
+
+
 
