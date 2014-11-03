2e39bd39f24259ca7fa27e8dd6e8c3ae769fcd1b
leisegang <leisegang@gmail.com>
2014-11-03 19:15:31 +0100

    Endret fil
    La til fil uten variabler

--- php/dbconfig.php
+++ php/dbconfig.php
@@ -1,13 +1,9 @@
 <?php
-
 /*
  * All database connection variables that the dbconnect.php uses to connedt to the MySQL server.
  */
-
-define('DB_USER', "patrick_utested"); // db user
-define('DB_PASSWORD', "11pwpvomec"); // db password (mention your db password here)
-define('DB_DATABASE', "patrick_utested"); // database name
+define('DB_USER', ""); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE', ""); // database name
 define('DB_SERVER', "localhost"); // db server
-
-
 ?>
