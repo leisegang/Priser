eca09a703f26029bf7613317de98877f6532e189
arodal <arodal91@gmail.com>
2014-11-03 20:08:41 +0100

    Delete dbconfig.php

--- php/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * All database connection variables that the dbconnect.php uses to connedt to the MySQL server.
- */
-define('DB_USER', ""); // db user
-define('DB_PASSWORD', ""); // db password (mention your db password here)
-define('DB_DATABASE', ""); // database name
-define('DB_SERVER', "localhost"); // db server
-?>
